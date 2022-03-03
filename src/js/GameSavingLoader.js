8db796daf208f6d762dd4ad04000f0c4c5d987af
SergeiSeledkov <gthemail92@gmail.com>
2022-03-03 20:42:30 +0500

    v2

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,8 +1,11 @@
 import parser from './parser';
 import reader from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
-    return reader().then(parser);
+    return reader()
+      .then((data) => parser(data))
+      .then((data) => new GameSaving(JSON.parse(data)));
   }
 }
